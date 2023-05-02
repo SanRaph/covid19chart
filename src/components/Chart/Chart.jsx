f7885604be3c4b54da6c0459049a9dfed6b17d66
SanRaph <raphytex@gmail.com>
2023-05-02 04:10:09 +0100

    ---

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import { fetchDailyData } from '../../api';
 
 const Chart = () => {
     const [dailyData, setDailyData] = useState({});
+
+    useEffect( () => {
+        const fetchAPI =  async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    });
+
     return(
         <hi>Chart</hi>
     )
